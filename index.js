93b975f000ed250b113207fd19af49a059900b9a
snogcel <ledniknoj@gmail.com>
2016-04-24 11:37:39 -0600

    adjust lib -> src

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var extend = require('webcoin-bitcoin')
 
 // inherit from bitcoin mainnet params
 module.exports = extend({
-  blockchain: require('./lib/blockchain.js'),
-  net: require('./lib/net.js'),
-  wallet: require('./lib/wallet.js')
+  blockchain: require('./src/blockchain.js'),
+  net: require('./src/net.js'),
+  wallet: require('./src/wallet.js')
 })
